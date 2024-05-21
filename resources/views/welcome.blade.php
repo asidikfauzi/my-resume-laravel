3c96bf874cd03117339a4bc1bf73adcb314d8609
asidikfauzi <asidikfauzi@gmail.com>
2024-05-21 17:55:51 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,13 +30,14 @@
                 <div class="card-block">
                     <div class="row">
                         <div class="col-md-12 order-md-1">
-                            <div class="d-flex justify-content-between">
+                            <div class="d-flex justify-content-between align-items-center">
                                 <div class="font-mono-2">
                                     <h2 class="font-bold">Ach Sidik Fauzi</h2>
-                                    <p>Software Engineer, Backend Developer focused on building products with extra attention to detail</p>
+                                    <p>Software Engineer, Backend Developer</p>
+                                    <p class="">Sumenep, 06-07-1999</p>
                                     <p>
-                                        <a class="font-mono" href="https://www.google.com/maps/place/Vonzy+Guesthouse/@-6.1509979,106.6998226,17z/data=!3m1!4b1!4m6!3m5!1s0x2e69f7f85cc61cd7:0x5bc40b4c3da826d8!8m2!3d-6.1509979!4d106.7023975!16s%2Fg%2F11v15z2264?entry=ttu" target="_blank" title="Google Maps">
-                                            <i class="fa fa-map-marker" aria-hidden="true"></i> Jakarta Barat 11840, Indonesia
+                                        <a class="" href="https://www.google.com/maps/place/Vonzy+Guesthouse/@-6.1509979,106.6998226,17z/data=!3m1!4b1!4m6!3m5!1s0x2e69f7f85cc61cd7:0x5bc40b4c3da826d8!8m2!3d-6.1509979!4d106.7023975!16s%2Fg%2F11v15z2264?entry=ttu" target="_blank" title="Google Maps">
+                                            <i class="fa fa-map-marker" aria-hidden="true"></i> West Jakarta 11840, Indonesia
                                         </a>
                                     </p>
                                     <p class="mt-3">
@@ -54,7 +55,7 @@
                                         </a>
                                     </p>
                                 </div>
-                                <span><img src="{{asset('assets/images/me.JPG')}}" class="img-responsive" alt=""></span>
+                                <span class="image-container"><img src="{{asset('assets/images/me.JPG')}}" class="img-responsive" alt=""></span>
                             </div>
                         </div>
                     </div>
@@ -88,16 +89,16 @@
                                     <p>Deployment Link: <a href="https://bookingtogo.com/" target="_blank">bookingtogo.com</a></p>
                                     <p>Implementing new features, creating APIs, cron, fixing bugs, and integrating data related to online travel agents. improve the performance of API responses. Technologies: Golang, Laravel, PostgreSQL, Redis, RabbitMQ, Docker</p>
                                 </div>
-                                <div class="col-md-12 font-mono mt-4">
+                                <div class="col-md-12 font-mono mt-3">
                                     <div class="d-flex justify-content-between">
                                         <p class="company-name"><b>Vorta Siber</b></p>
                                         <span>Mar 2022 - May 2023</span>
                                     </div>
                                     <p class="job-title">Secret Project | Backend Developer</p>
                                     <p>Built a notes application used by agencies that can identify people's faces from photos based on facial criteria using nodedeflux. Technologies: Node.js, PostgreSQL, Redis, RabbitMQ, Docker, Kong API Gateway for microservice architecture</p>
-                                    <p class="job-title mt-3">OneBlock | Backend Developer</p>
+                                    <p class="job-title mt-2">OneBlock | Backend Developer</p>
                                     <p>Migrate from nest.js to golang where this application performs an audit trail of application usage using blockchain. Technologies: Golang, Node.js, PostgreSQL</p>
-                                    <p class="job-title mt-3">Sicana Kejaksaan Agung RI | Backend Developer</p>
+                                    <p class="job-title mt-2">Sicana Kejaksaan Agung RI | Backend Developer</p>
                                     <p>Input Tools Deployment Link: <a href="https://sicana.kejaksaan.go.id/" target="_blank">sicana.kejaksaan.go.id</a></p>
                                     <p>Dashboard Deployment Link: <a href="https://dashboard.sicana.kejaksaan.go.id/login" target="_blank">dashboard.sicana.kejaksaan.go.id</a></p>
                                     <p>Contribute to creating applications that facilitate integrated planning and budgeting in accordance with the needs of Prosecutor's work units throughout Indonesia. Technologies: Laravel, Node.js, PostgreSQL</p>
@@ -138,33 +139,37 @@
                         </div>
                     </div>
                 </div>
-                <div class="card-block mb-3">
+                <div class="card-block d-flex flex-column">
                     <h3 class="font-bold mb-3">Projects</h3>
                     <div class="row font-mono">
-                        <div class="col-md-12 font-mono-project">
-                            <div class="d-flex justify-content-between">
-                                <p class="company-name"><b>Diaspora Muda Sumenep</b></p>
-                                <span>Jan 2024 - Present</span>
+                        <div class="col-md-12 p-5">
+                            <div class="p-border">
+                                <div class="d-flex justify-content-between">
+                                    <p class="company-name"><b>Diaspora Muda Sumenep</b></p>
+                                    <span>Jan 2024 - Present</span>
+                                </div>
+                                <p class="job-title">My Organization Products | Backend Developer</p>
+                                <p>Deployment Link: <a href="https://diasporamudasumenep.com/" target="_blank">diasporamudasumenep.com</a></p>
+                                <p>Home of Diaspora Muda Sumenep, A youth forum for young immigrants from Sumenep Regency to contribute to local development. Focuses on developing human resources through seminars, training, cadre formation, and networking</p>
+                                <div class="skill-v2">Lead Backend Developer</div>
+                                <div class="skill-v2">Lumen/Laravel</div>
+                                <div class="skill-v2">MySQL</div>
+                                <div class="skill-v2">Docker</div>
                             </div>
-                            <p class="job-title">My Organization Products | Backend Developer</p>
-                            <p>Deployment Link: <a href="https://diasporamudasumenep.com/" target="_blank">diasporamudasumenep.com</a></p>
-                            <p>Home of Diaspora Muda Sumenep, The Sumenep Youth Diaspora is a forum for young immigrants from Sumenep Regency in various regions in Indonesia to make real contributions to the development and progress of Sumenep Regency. This forum focuses on developing Human Resources (HR) in Sumenep Regency, especially the young generation who will manage Sumenep Regency in the future. Various activities such as seminars, training, cadre formation, networking and other positive activities to develop Human Resources (HR) in Sumenep Regency are the focus of this forum</p>
-                            <div class="skill-v2">Lead Backend Developer</div>
-                            <div class="skill-v2">Lumen/Laravel</div>
-                            <div class="skill-v2">MySQL</div>
-                            <div class="skill-v2">Docker</div>
                         </div>
-                        <div class="col-md-12 font-mono-project">
-                            <div class="d-flex justify-content-between">
-                                <p class="company-name"><b>Real-time Sales Monitoring and Attendance System</b></p>
-                                <span>Dec 2021 - Mar 2022</span>
+                        <div class="col-md-12 p-5">
+                            <div class="p-border">
+                                <div class="d-flex justify-content-between">
+                                    <p class="company-name"><b>Sales Tap</b></p>
+                                    <span>Dec 2021 - Mar 2022</span>
+                                </div>
+                                <p class="job-title">Freelance | Backend Developer</p>
+                                <p>APK Deployment Link: <a href="https://install.appcenter.ms/users/ariefyusron98/apps/salestap/distribution_groups/internal%20test" target="_blank">apk.sales.tap</a></p>
+                                <p>CMS Deployment Link: <a href="http://103.191.92.76/store" target="_blank">cms.sales.tap</a></p>
+                                <p>Our system ensures accurate sales attendance and provides the ability to monitor sales activities in a timely manner. Sales can easily take attendance with photos and location notes every time they visit a target store. This helps ensure transparency in field operations and allows management to track sales performance effectively</p>
+                                <div class="skill-v2">Lumen/Laravel</div>
+                                <div class="skill-v2">MySQL</div>
                             </div>
-                            <p class="job-title">Freelance | Backend Developer</p>
-                            <p>APK Deployment Link: <a href="https://install.appcenter.ms/users/ariefyusron98/apps/salestap/distribution_groups/internal%20test" target="_blank">apk.sales.tap</a></p>
-                            <p>CMS Deployment Link: <a href="http://103.191.92.76/store" target="_blank">cms.sales.tap</a></p>
-                            <p>Our system ensures accurate sales attendance and provides the ability to monitor sales activities in a timely manner. Sales can easily take attendance with photos and location notes every time they visit a target store. This helps ensure transparency in field operations and allows management to track sales performance effectively</p>
-                            <div class="skill-v2">Lumen/Laravel</div>
-                            <div class="skill-v2">MySQL</div>
                         </div>
                     </div>
                 </div>
@@ -172,6 +177,6 @@
         </div>
     </div>
 </main>
---><script type="text/javascript" src="{{asset('main.bc58148c.js')}}"></script></body>
+<script type="text/javascript" src="{{asset('main.bc58148c.js')}}"></script></body>
 
 </html>
